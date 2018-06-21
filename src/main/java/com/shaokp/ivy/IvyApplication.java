2929d6307656d844d5d4dc08fe02d069f1be4ce5
kp.shao <kp.shao@outlook.com>
2018-06-21 20:52:54 +0800

    [chore]: init project

---
+++ src/main/java/com/shaokp/ivy/IvyApplication.java
@@ -0,0 +1,12 @@
+package com.shaokp.ivy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IvyApplication.class, args);
+    }
+}
